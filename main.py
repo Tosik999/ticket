c751f3e4d147a1ed8c0c1647544ff0a8d9200db5
Tosik999 <80121206+Tosik999@users.noreply.github.com>
2024-01-14 23:55:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def handle_request():
     param1 = request.args.get('date')
     param2 = request.args.get('passengers')
     param3 = request.args.get('route')
-    return [param1, param2, param3]
+    return 's'
 
 if __name__ == '__main__':
     app.run()
