063c3df8d40eb0ff5e3428bba15bf5820e1636aa
Tosik999 <80121206+Tosik999@users.noreply.github.com>
2024-01-14 22:14:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/ticket')
 def ticket():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ("127.0.0.1", 12345)
+    server_address = ("https://flask-production-eeed.up.railway.app/ticket", 5000)
     server_socket.bind(server_address)
     server_socket.listen(1)
 
